d0b9ba4189ca3a3704d724e2546b8f98fe4166d4
Prashant Singh <97303117+hiprashantsingh@users.noreply.github.com>
2025-06-05 17:17:03 +0530

    Add files via upload

---
+++ 1web.js
@@ -0,0 +1,13 @@
+// Sidebar open/close
+function toggleSidebar() {
+    document.getElementById("sidebar").classList.toggle("active");
+}
+
+// Dropdown toggle
+function toggleDropdown() {
+    document.getElementById("dropdown").classList.toggle("open");
+}
+
+function toggleLoginPopup() {
+    document.getElementById("loginPopup").classList.toggle("active");
+}
